0dd99d1c74ab4b8b4910a69360a84c7ae9390841
mateusflawer <mateusflawer@gmail.com>
2025-05-25 19:16:04 -0300

    Add .env.example file and update config to load environment variables

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,12 @@
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class Settings(BaseSettings):
-    SECRET_KEY: str = "your-secret-key-here"  # Substitua por uma chave segura
+    # Substitua por uma chave segura
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "your-secret-key-here")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     DATABASE_URL: str = "sqlite:///./users.db"
